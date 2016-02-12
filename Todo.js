13f0bd4740f36413e27b39de079a12a5441c93c6
Ashish Agnihotri <vinicreates@gmail.com>
2016-02-12 08:30:51 -0800

    Angular JS Todo :Initial Commit

---
+++ Todo.js
@@ -0,0 +1,53 @@
+var todoModule = angular.module("todoApp", []);
+todoModule.controller("todoController", todoFn);
+
+function todoFn($scope){
+     $scope.arrItems = [];
+     $scope.setToDelete = [];
+
+
+    $scope.addItemFn = function (){
+       if($scope.arrItems.indexOf($scope.taskItem) > -1) {
+             alert("This task already exists. Please add another task.");
+             $scope.taskItem = '';
+           return;
+       }
+        $scope.arrItems.push($scope.taskItem);
+        $scope.taskItem = '';
+        $scope.setToDelete.push(false);
+      }
+   
+   
+   $scope.deleteItemFn = function(index){
+      $scope.arrItems.splice(index,1);
+      $scope.setToDelete.splice(index,1);
+   }
+   
+   
+   $scope.deleteAllItems = function() {
+       $scope.arrItems = [];
+       $scope.setToDelete = [];
+   }
+   
+   
+   $scope.setItemToDelete = function(index){
+       if( !$scope.setToDelete[index])
+        $scope.setToDelete[index] = true;
+       else
+        $scope.setToDelete[index] = false;
+     }
+   
+   $scope.deleteSelectedItems = function(){
+       var newArr = [];
+       var newArrDel = [];
+       for(var i=0; i<=$scope.arrItems.length; i++) {
+          if($scope.setToDelete[i] === false){
+                newArr.push($scope.arrItems[i]);
+                newArrDel.push(false);
+          }
+       }
+       $scope.arrItems = newArr;
+       $scope.setToDelete = newArrDel;
+   }
+   
+}
